a82b885c89c999aace7271aed26026a4ef11eca0
reflying <278432993@qq.com>
2016-06-17 09:48:21 +0800

    修改配置

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# 移动运行环境
+# 移动运行环境 
 
 mysql:5.6.30
 apache:
 php:
 memcached:
+
+#启动容器
+
+./start.sh
+apache配置文件路径
+/etc/apache2/sites-enabled/
 
--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
-
+#!/bin/bash
+# 
+# 
+# USAGES: ./star.sh
 docker-compose up -d
-cname=mysql
+cname=ibbd-cmccproject
 docker exec  $cname  sh -c 'exec /etc/init.d/apache2 start'
 docker exec  $cname  sh -c 'exec memcached -d -u memcache'
 # docker exec  $cname  sh -c 'exec php5-fpm -D -c /usr/local/etc/php/php.ini -y /usr/local/etc/php-fpm.conf'
